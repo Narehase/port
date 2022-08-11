ee742a7c44152c00af33be639666c4ae73b90861
Narehase <99946234+Narehase@users.noreply.github.com>
2022-08-11 13:55:34 +0900

    Create port.py

---
+++ port.py
@@ -0,0 +1,13 @@
+import serial.tools.list_ports as sp
+
+list = sp.comports()
+
+connected = []
+
+
+
+for i in list:
+
+  connected.append(i.device)
+
+print("Connected COM ports: " + str(connected))
